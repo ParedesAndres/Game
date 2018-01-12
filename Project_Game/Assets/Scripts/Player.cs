496e3531471e80faae62268ffedf3276eefcb8a6
ParedesAndres <35270955+ParedesAndres@users.noreply.github.com>
2018-01-12 17:35:06 -0500

    TO DO
    Ampliar el tamaño del plano ----
    Crear una elipse para el personaje ----
    Crear la tabla de valores publicas(De esta manera se puede moficar) ----
    Crear un material para el personaje ----

---
+++ Project_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	//Use this for changes the values 
+	//The first character is a shooter
+	//Set the inicial values in Start()
+	public float damage;
+	public float attack_velocity;
+	public float health;
+	public float magic_power;
+	public float armor;
+
+
+	// Use this for initialization
+	void Start () {
+		//Incial values
+		damage = 35;
+		attack_velocity = 40;
+		health = 100;
+		magic_power = 10;
+		armor = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
