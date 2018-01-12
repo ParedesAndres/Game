cbebce5c6b82c3ecba4d96a07f8097dbfd8fd936
ParedesAndres <35270955+ParedesAndres@users.noreply.github.com>
2018-01-12 13:02:48 -0500

    Trying father class
    Typing  the C# struct

---
+++ Project_Game/Assets/Scripts/FatherPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatherPlayer{
+
+	protected float velocidad;
+
+	public FatherPlayer(float velocidad) {
+		this.velocidad = velocidad;
+	}
+
+
+	public virtual void  disparar() {
+		velocidad = 10;
+	}
+}
